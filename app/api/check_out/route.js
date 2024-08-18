1df28b6e26584a9fa15f838961269819bec1e5ee
Rafidr2 <rafidr10@gmail.com>
2024-08-18 16:13:55 -0400

    finished route.js in checkout session

--- app/api/check_out/route.js
+++ app/api/check_out/route.js
@@ -2,21 +2,30 @@ import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 export async function POST(req){
-    const params: = {
+    const params = {
         submit_type: 'subsciption',
         payment_method_types: ['card'],
         line_items: [
           {
-            name: ' ',
-            amount: formatAmountForStripe(amount, CURRENCY),
-            currency: CURRENCY,
-            quantity: 1,
+           price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Pro Subscription',
+            },
+            unit_amount: formatAmountForStripe(10),
+           
           },
+          
+          quantity: 1,
+        },
         ],
         success_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${req.headers.origin}/result?session_id={CHECKOUT_SESSION_ID}`,
-      };
-      const checkoutSession:  =
+      }
+      const checkoutSession  =
         await stripe.checkout.sessions.create(params);
 
+        return NextResponse.json(checkoutSession, {
+          status:200,
+        })
 }
