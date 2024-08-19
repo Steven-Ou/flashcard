43440bd4afe2e514980712f57e9538d87b51e280
Steven-Ou <osteve425@gmail.com>
2024-08-18 23:42:28 -0400

    Signed in and signout userbutton

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
-
+import getStripe from "@/utils/get-stripe";
+import {SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
 
 export default function Home() {
   return (
