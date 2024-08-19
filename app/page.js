e4eab3280a38d3d5f83a585722cc485b5609bc4e
Steven-Ou <osteve425@gmail.com>
2024-08-18 23:58:36 -0400

    Typography

--- app/page.js
+++ app/page.js
@@ -1,12 +1,25 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import {SignedIn, SignedOut, UserButton} from '@clerk/nextjs'
-import { Container } from "@mui/material";
+import { AppBar, Container, Toolbar, Typography } from "@mui/material";
+import Head from "next/head";
 
 export default function Home() {
   return (
    <Container maxWidth="lg">
-    
+    <Head>
+      <title> Flashcard </title>
+      <meta name="description" content="Create flashcard from your test" />
+    </Head>
+
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6"> 
+          Flashcard
+          <
+        </Typography>
+      </Toolbar>
+    </AppBar>
    </Container>
   )
 }
