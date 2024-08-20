835ef8594b932ca8555289b65789fa0228276540
Steven-Ou <osteve425@gmail.com>
2024-08-20 12:01:05 -0400

    word for button

--- app/page.js
+++ app/page.js
@@ -97,7 +97,7 @@ export default function Home() {
             Access to basic flashcard Feature and limited storage.
           </Typography>
           <Button variant="contained" color="primary" sx={{mt:2}}>
-
+            Choose basic
           </Button>
         </Box>
         </Grid>
