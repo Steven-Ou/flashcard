97f990ddc8708d82d08923ae94a4da4d67a44b95
Steven-Ou <osteve425@gmail.com>
2025-10-14 19:51:34 -0400

    structuring it

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
-import dynamic from 'next/dynamic';
-import { CircularProgress, Box } from '@mui/material';
+import dynamic from "next/dynamic";
+import { CircularProgress, Box } from "@mui/material";
 
-const HomeComponent = dynamic(() => import('.app/Home'), {
+const HomeComponent = dynamic(() => import(".app/Home"), {
   ssr: false,
   loading: () => (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
       <CircularProgress />
     </Box>
   ),
